a117a5d39a0fe49e40d685f5034f42eeaaeabda1
Bruno Fernandes <bruno.enrico99@gmail.com>
2025-01-21 00:46:28 -0300

    Registration process completed

--- djangocrm/website/views.py
+++ djangocrm/website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 
 def home(request):
@@ -24,3 +25,23 @@ def logout_user(request):
     logout(request=request)
     messages.success(request, "You have been logged out!")
     return redirect('home')
+
+def register_user(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and log in
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user=user)
+            messages.success(request=request, message="Your account have been created successfully!")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form': form})
+    return render(request, 'register.html', {'form': form})
+
+def admin():
+    return redirect('admin')
