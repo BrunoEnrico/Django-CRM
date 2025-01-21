fe015b24c2a319e238b67c21ac0875d9f16b558b
Bruno Fernandes <bruno.enrico99@gmail.com>
2025-01-21 11:46:52 -0300

    Add new records functionality added

--- djangocrm/website/forms.py
+++ djangocrm/website/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from .models import Record
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class': 'form_control', 'placeholder': 'Email Address'}))
@@ -32,4 +33,32 @@ class SignUpForm(UserCreationForm):
         self.fields['password2'].widget.attrs['class'] = 'form-control'
         self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
         self.fields['password2'].label = ''
-        self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'	
+        self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'
+
+
+class AddRecordForm(forms.ModelForm):
+
+    first_name = forms.CharField(label="", max_length=50, widget=forms.TextInput(attrs={'class': 'form_control', 'placeholder': 'First Name'}))
+    last_name = forms.CharField(label="", max_length=50 ,widget=forms.TextInput(attrs={'class': 'form_control', 'placeholder': 'Last Name'}))
+    email = forms.CharField(label="", max_length=50 ,widget=forms.TextInput(attrs={'class': 'form_control', 'placeholder': 'Email'}))
+    phone = forms.CharField(label="", max_length=50 ,widget=forms.TextInput(attrs={'class': 'form_control', 'placeholder': 'Phone'}))
+    address = forms.CharField(label="", max_length=50 ,widget=forms.TextInput(attrs={'class': 'form_control', 'placeholder': 'Address'}))
+    city = forms.CharField(label="", max_length=50 ,widget=forms.TextInput(attrs={'class': 'form_control', 'placeholder': 'City'}))
+    state = forms.CharField(label="", max_length=50 ,widget=forms.TextInput(attrs={'class': 'form_control', 'placeholder': 'State'}))
+    zip_code = forms.CharField(label="", max_length=50 ,widget=forms.TextInput(attrs={'class': 'form_control', 'placeholder': 'Zip Code'}))
+
+    def __init__(self, *args, **kwargs):
+        super(AddRecordForm, self).__init__(*args, **kwargs)
+        self.fields["first_name"].widget.attrs["class"] = "form-control"
+        self.fields["last_name"].widget.attrs["class"] = "form-control"
+        self.fields["email"].widget.attrs["class"] = "form-control"
+        self.fields["phone"].widget.attrs["class"] = "form-control"
+        self.fields["address"].widget.attrs["class"] = "form-control"
+        self.fields["city"].widget.attrs["class"] = "form-control"
+        self.fields["state"].widget.attrs["class"] = "form-control"
+        self.fields["zip_code"].widget.attrs["class"] = "form-control"
+
+
+    class Meta:
+        model = Record
+        exclude = ("user",)
--- djangocrm/website/views.py
+++ djangocrm/website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
 
@@ -63,6 +63,18 @@ def delete_record(request, pk):
         messages.success(request=request, message="You must be logged in to do that!")
         return redirect('home')
 
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            if form.is_valid():
+                form.save()
+                messages.success(request=request, message="Record added successfully!")
+                return redirect('home')
+        return render(request, 'add_record.html', {'form': form})
+    else:
+        messages.success(request=request, message="You must be logged in to do that!")
+        return redirect('home')
 
 
 def admin():
